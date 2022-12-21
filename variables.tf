bfe77f049d1da47b53d71a59054ad60e239d3e66
Silpashree <silpashree@bootlabstech.com>
2022-12-21 11:18:47 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "key_vault_name"{
+    type = string
+    description = "key vault name"
+}
+variable "location"{
+    type = string
+    description = "location of the key vault"
+}
+variable "resource_group"{
+    type = string
+    description = "resource group of the key vault"
+}
+variable "soft_delete_retention_days"{
+    type = number
+    description = "how much is the soft delete retention days for the key vault"
+}
+variable "purge_protection_enabled"{
+    type = bool
+    description = "whether purge_protection is enabledfor the key vault"
+}
+variable "sku_name"{
+    type = string
+    description = "sku for the key vault"
+}
