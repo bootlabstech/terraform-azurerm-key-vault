9a14fecefd1847392033d23e254c082c6a78c111
lalitha2407 <lalitha@bootlabstech.com>
2023-03-09 13:45:31 +0530

    rbac_authorization disabled

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_key_vault" "key_vault" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = var.soft_delete_retention_days
   purge_protection_enabled    = var.purge_protection_enabled
-  enable_rbac_authorization   = true
+  enable_rbac_authorization   = false
   sku_name                    = var.sku_name
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
