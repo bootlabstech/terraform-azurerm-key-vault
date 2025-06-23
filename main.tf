b0a65d0faedf69a925fe6e1f28f4dd08b0e53815
Bhargava Naidu <bhargava@bootlabstech.com>
2025-06-23 21:49:45 +0530

    updated the args

--- main.tf
+++ main.tf
@@ -3,13 +3,16 @@ resource "azurerm_key_vault" "key_vault" {
   name                        = var.name
   location                    = var.location
   resource_group_name         = var.resource_group_name
-  enabled_for_disk_encryption = true
+  sku_name                    = var.sku_name
+  enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+  enabled_for_deployment          = var.enabled_for_deployment
+  enable_rbac_authorization   = var.enable_rbac_authorization
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = var.soft_delete_retention_days
   purge_protection_enabled    = var.purge_protection_enabled
-  enable_rbac_authorization   = false
   public_network_access_enabled = var.public_network_access_enabled
-  sku_name                    = var.sku_name
+
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
@@ -26,6 +29,10 @@ resource "azurerm_key_vault" "key_vault" {
   network_acls {
     bypass = "AzureServices"
     default_action = "Allow"
-
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
   }
 }
--- variables.tf
+++ variables.tf
@@ -10,6 +10,31 @@ variable "resource_group_name" {
   type        = string
   description = "resource group of the key vault"
 }
+variable "sku_name" {
+  type        = string
+  description = "sku for the key vault"
+}
+
+variable "enabled_for_deployment" {
+  type        = bool
+  description = "Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault."
+  default = true
+}
+variable "enabled_for_disk_encryption" {
+  type        = bool
+  description = " Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys."
+  default = true
+}
+variable "enabled_for_template_deployment" {
+  type        = bool
+  description = "Boolean flag to specify whether Azure Resource Manager is permitted to retrieve secrets from the key vault."
+  default = true
+}
+variable "enable_rbac_authorization" {
+  type        = bool
+  description = "Boolean flag to specify whether Azure Key Vault uses Role Based Access Control (RBAC) for authorization of data actions."
+  default = false
+}
 variable "soft_delete_retention_days" {
   type        = number
   description = "how much is the soft delete retention days for the key vault"
@@ -20,11 +45,9 @@ variable "purge_protection_enabled" {
   description = "whether purge_protection is enabledfor the key vault"
   default = true
 }
-variable "sku_name" {
-  type        = string
-  description = "sku for the key vault"
-}
+
 variable "public_network_access_enabled" {
-  type        = string
+  type        = bool
   description = "monitor_name for the key vault"
+  default = true
 }
